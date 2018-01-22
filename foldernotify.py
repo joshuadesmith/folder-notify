d9480a4b99bb942bbd42dbbf0ce8a33bc43af3dd
Joshua Smith <joshuadesmith@utexas.edu>
2018-01-22 17:21:26 -0600

    added function to pull files in intern working directory

--- foldernotify.py
+++ foldernotify.py
@@ -1,17 +1,31 @@
 # foldernotify.py
 # Author: Josh Smith
 
-filename = "contact.txt"
+import os
 
-def get_contacts(filename):
+contact_filename = 'contact.txt'
+folder_filename = 'fdir.txt'
+
+def get_contacts():
 	contacts = []
-	with open(filename, mode='r', encoding='utf-8') as contact_file:
+	with open(contact_filename, mode='r', encoding='utf-8') as contact_file:
 		for contact in contact_file:
 			contacts.append(contact)
 	return contacts
 
 
+# Gets contents of folder that contains intern tasks
+def get_folder_contents():
+	fdir = open(folder_filename, mode='r', encoding='utf-8').read().splitlines()[0]
+	files = [f for f in os.listdir(fdir) if os.path.isfile(f)]
+	return files
+
+
 if __name__ == "__main__":
-	for contact in get_contacts(filename):
-		print(contact)
+	print("Contacts:")
+	for contact in get_contacts():
+		print("\t- " + contact)
+	print("Folder Contents:")
+	for file in get_folder_contents():
+		print("\t- " + file)
 		
