9cd05e16e41aca4c6f1bf8f7bc1431b760019179
Joshua Smith <joshuadesmith@utexas.edu>
2018-01-23 09:44:16 -0600

    implemented function to get files that have been added or removed from the working directory

--- foldernotify.py
+++ foldernotify.py
@@ -20,6 +20,38 @@ def get_folder_contents():
 	files = [f for f in os.listdir(fdir) if os.path.isfile(f)]
 	return files
 
+# Gets difference in intern task folder
+# Returns 1) new folder contents 2) list of added files, 3) list of removed files
+def get_folder_diff(folder_contents):
+	added = []
+	removed = []
+	new_folder_contents = get_folder_contents()
+	if folder_contents:
+		if not new_folder_contents:
+			removed = folder_contents
+		else:
+			removed = [f for f in folder_contents if f not in new_folder_contents]
+			added = [f for f in new_folder_contents if f not in folder_contents]
+	else:
+		if folder_contents:
+			added = folder_contents;
+	return new_folder_contents, added, removed
+
+
+# Main function
+# wait_time = number of minutes to wait before iterating loop
+# TODO: Implement wait function
+# TODO: Implement notify function
+def main():
+	current_folder_contents = get_folder_contents()
+	while True:
+		# wait()
+		current_folder_contents, added, removed = get_folder_diff(current_folder_contents)
+		if added:
+			# notify(current_folder_contents, added, removed)
+		else:
+			# TODO: print message indicating no new files
+
 
 if __name__ == "__main__":
 	print("Contacts:")
